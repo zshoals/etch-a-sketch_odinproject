e388db3b439001adba68e6e6d12de50591731cee
zshoals <zshoals+github@gmail.com>
2019-07-14 14:35:47 -0500

    Initial commit. HTML init, script init.

---
+++ scripts/etch-board.js
@@ -0,0 +1,36 @@
+//Etch-a-sketch steps
+
+let numberOfSquares = 60;
+let containerLength = 100;
+let containerHeight = 100;
+let divArray = null;
+    
+//Generate a list of divs and store it
+//In some sort of array
+for (let i = 0; i < numberOfSquares; i++) {
+    //Generate Div
+    divArray.push()
+    
+    //attach it to a screen container
+
+
+    //========================================
+
+    //For the div list, assign a class for styling
+    //and any other general style information
+
+    //========================================
+
+    //For the div list, calculate size based on
+    //the container size for width and height
+    //Formula: SquareLen = ContainerLen / NumberOfSquares
+
+    //========================================
+
+    //Add event listeners for every div to check for mouse overlap
+    //if detected, call either a color or randColor function
+    //depending on script state
+
+    //========================================
+
+}
